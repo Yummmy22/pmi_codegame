983c7f49a55ad1deb31d412be7939c860568b98a
y4nayana <krisna.udayana79@gmail.com>
2024-03-16 23:11:02 +0700

    Timer Score

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] GameObject starPrefab; // Prefab UI Image untuk bintang
+    [SerializeField] Transform starParent; // Parent untuk semua bintang
     [SerializeField] float initialTime;
+    [SerializeField] int maxStars; // Jumlah maksimum bintang yang mungkin
     private float remainingTime;
     private bool isTimerActive = false;
+    private int score;
 
     void Update()
     {
@@ -24,7 +29,7 @@ public class Timer : MonoBehaviour
                 remainingTime = 0;
                 // GameOver();
                 timerText.color = Color.red;
-                // Matikan timer setelah mencapai 0 (opsional)
+                CalculateScore(); // Hitung skor saat permainan berakhir
                 isTimerActive = false;
             }
 
@@ -42,6 +47,44 @@ public class Timer : MonoBehaviour
                 remainingTime = initialTime;
                 isTimerActive = true;
             }
+        }
+    }
+
+    // Method untuk menghitung skor
+    void CalculateScore()
+    {
+        float timePercentage = remainingTime / initialTime; // Hitung persentase waktu tersisa
+
+        // Tentukan jumlah bintang berdasarkan rentang waktu
+        if (timePercentage >= 0.67f) // 67% atau lebih, 3 bintang
+        {
+            score = 3;
+        }
+        else if (timePercentage >= 0.33f) // 33% atau lebih, 2 bintang
+        {
+            score = 2;
+        }
+        else // Kurang dari 33%, 1 bintang
+        {
+            score = 1;
+        }
+
+        DisplayStars(score); // Tampilkan bintang sesuai dengan skor
+    }
+
+    // Method untuk menampilkan bintang
+    void DisplayStars(int numberOfStars)
+    {
+        // Hapus semua bintang sebelum menampilkan yang baru
+        foreach (Transform child in starParent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Instantiate UI Image bintang sejumlah numberOfStars
+        for (int i = 0; i < numberOfStars; i++)
+        {
+            Instantiate(starPrefab, starParent);
         }
     }
 }
