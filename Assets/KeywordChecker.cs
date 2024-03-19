b9781505cdecdf326259810bfaebf958813af2da
greenstar <89572173+greenstarishere@users.noreply.github.com>
2024-03-20 01:46:18 +0700

    easy bat ini mah

--- Assets/KeywordChecker.cs
+++ Assets/KeywordChecker.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+
+
+[System.Serializable]
+public class KeywordEvents
+{
+    public string Keyword;
+    public UnityEvent OnKeywordMatch;
+}
 
 public class KeywordChecker : MonoBehaviour
 {
+    public TMP_InputField InputField;
+    public CalculationString m_string;
+    public List<KeywordEvents> KeywordEvents;
+    public UnityEvent FalseEvent;
+    bool ConditionHasMatched;
     // Start is called before the first frame update
-    void Start()
+    public void CheckKeywords()
     {
-        
-    }
+        print(m_string.CurrentValue);
+        foreach (var keywordEvent in KeywordEvents)
+        {
+            if (keywordEvent.Keyword == m_string.CurrentValue)
+            {
+                keywordEvent.OnKeywordMatch.Invoke();
+                ConditionHasMatched = true;
+                InputField.text = "";
+                break;
+            }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+        if (!ConditionHasMatched)
+        {
+            FalseEvent.Invoke();
+        }
+        ConditionHasMatched = false; //reset condition
     }
 }
